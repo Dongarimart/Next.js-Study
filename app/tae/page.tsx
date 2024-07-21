3d2745017fb81ce7b92aa50e3123f91235d78bc5
taekoong <kyung3900@gmail.com>
2024-07-21 20:22:01 +0900

    a

--- app/tae/page.tsx
+++ app/tae/page.tsx
@@ -1,12 +1,31 @@
+'use client';
+
+import { useState } from "react";
+
 import styles from "./page.module.css";
 
 export default function Tae() {
+
+  const [pizzaCount, setPizzaCount] = useState(1);
+
+  const handleButtonClick = () => {
+    setPizzaCount(pizzaCount+1);
+  };
+
   return (
     <div className={styles.container}>
-      <h1>배가너무부름</h1>
+      <button className={styles.button} onClick={handleButtonClick}>
+        배가너무부름
+        </button>
       <div className={styles.progressBar}>
-        🍕
+        {"🍕".repeat(pizzaCount)}
+      </div>
+      <div className={styles.buttonContainer}>
+        <button onClick={() => setPizzaCount(1)}>Reset</button>
+        <a href="/">
+          <button>Home</button>
+        </a>
       </div>
     </div>
-  )
+  );
 }
