b1bf73be7992bec21d081fadfb6e82f95d89ce9c
taekoong <kyung3900@gmail.com>
2024-08-04 20:33:08 +0900

    summary

--- app/tae/page.tsx
+++ app/tae/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./page.module.css";
 import Link from 'next/link';
 import ProgressBar from './components/ProgressBar';
@@ -25,14 +25,27 @@ export default function Tae() {
       progress: 1,
       icon: '🖐️',
     },
-    tae: {
+    jhyun: {
       progress: 1,
-      icon: '🍕',
+      icon: '⛄',
     },
   }
 
   // TODO - useEffect, setPeopleProgress로 나와 다른사람의 게이지 API로 받아오기
   const [peopleProgress, setPeopleProgress] = useState(people);
+
+  useEffect(() => {
+    const fetchProgress = async () => {
+      try {
+        const response = await axios.get('/api/getProgress');
+        setPeopleProgress(response.data);
+      } catch (error) {
+        console.error('Error', error);
+      }
+    };
+
+    fetchProgress();
+  }, []);
 
   const handleButtonClick = () => {
     if (pizzaCount < MAX_PIZZA){
@@ -60,15 +73,17 @@ export default function Tae() {
     {/* 다른 사람들 게이지 */}
     {/* TODO - 다른 3명의 게이지 UI/UX 완성하기 */}
     <div className={styles.container}>
-        {
+        {Object.keys(peopleProgress).map((key) => (
+          <ProgressBar
+          key={key}
+          name={key}
+          icon={peopleProgress[key].icon} 
+          count={peopleProgress[key].progress}
+        />
+        ))
           // 반복되는 요소는 Component로 만들어 조립할 수 있다
           // ProgressBar Component 파일 위치: ./components/ProgressBar.tsx
           // ProgressBar 파일 편집해도 상관 없음.
-          <ProgressBar
-            name={'hoon'}
-            icon={peopleProgress.hoon.icon} 
-            count={peopleProgress.hoon.progress}
-          />
         }
       </div>
   </>
